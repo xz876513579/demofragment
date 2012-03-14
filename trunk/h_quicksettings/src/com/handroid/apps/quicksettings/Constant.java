2617ab84afedaa7e0ca099a9778c125b250311e6
vothanhnhat85@gmail.com <vothanhnhat85@gmail.com@6724584e-4cce-c52b-7cf4-d499e0221991>
2012-03-14 17:14:12 +0000

    update app setting!

--- trunk/h_quicksettings/src/com/handroid/apps/quicksettings/Constant.java
+++ trunk/h_quicksettings/src/com/handroid/apps/quicksettings/Constant.java
@@ -3,4 +3,5 @@ package com.handroid.apps.quicksettings;
 public interface Constant {
 	public static final String PREF_NAME = "quick_settings_pref";
 	public static final String PREF_WALL_POS = "pref_wall_pos";
+	public static final String PREF_CHECK_SHOW_NOTIFICATION = "pref_check_show_notification";
 }
