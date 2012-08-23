8a67ef3e4973bc825d41f9e97e2bc972c880989a
vothanhnhat85@gmail.com <vothanhnhat85@gmail.com@6724584e-4cce-c52b-7cf4-d499e0221991>
2012-08-23 04:00:38 +0000

    import new test demo project!

---
+++ trunk/YourStyle/src/com/handroid/apps/yourstyle/MainActivity.java
@@ -0,0 +1,22 @@
+package com.handroid.apps.yourstyle;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_layout, menu);
+        return true;
+    }
+
+    
+}
