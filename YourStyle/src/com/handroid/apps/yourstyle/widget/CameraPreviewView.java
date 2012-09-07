c9f4d3d770b84fb532e5718abd573ecfd23d2e19
vothanhnhat85 <vothanhnhat85@gmail.com>
2012-09-07 10:35:12 +0000

    Update code!

--- YourStyle/src/com/handroid/apps/yourstyle/widget/CameraPreviewView.java
+++ YourStyle/src/com/handroid/apps/yourstyle/widget/CameraPreviewView.java
@@ -1,24 +1,23 @@
 package com.handroid.apps.yourstyle.widget;
 
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Bitmap.CompressFormat;
 import android.hardware.Camera;
-import android.hardware.Camera.Parameters;
 import android.hardware.Camera.Size;
+import android.os.ConditionVariable;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.OrientationEventListener;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class CameraPreviewView extends SurfaceView implements SurfaceHolder.Callback {
+
+    private /*static*/ final String TAG = "camera_review";
 
     public CameraPreviewView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -45,10 +44,13 @@ public class CameraPreviewView extends SurfaceView implements SurfaceHolder.Call
     }
     
     Camera.PictureCallback mPictureCallback = new Camera.PictureCallback() {
+        private ConditionVariable mSig = new ConditionVariable();
         public void onPictureTaken(byte[] imageData, Camera camera) {
             if (imageData != null) {
                 storeByteImage(mContext, imageData, 100,
                         "ImageName");
+                // start preview again
+                mCamera.startPreview();
             }
         };
     };
@@ -56,7 +58,7 @@ public class CameraPreviewView extends SurfaceView implements SurfaceHolder.Call
     public /*static*/ boolean storeByteImage(Context mContext, byte[] imageData,
             int quality, String expName) {
 
-        File sdImageMainDirectory = new File("/sdcard");
+        /*File sdImageMainDirectory = new File("/sdcard");
         FileOutputStream fileOutputStream = null;
         try {
             BitmapFactory.Options options=new BitmapFactory.Options();
@@ -80,8 +82,22 @@ public class CameraPreviewView extends SurfaceView implements SurfaceHolder.Call
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        }*/
+        String path = "/sdcard/image.jpg";
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(new File(path), false);
+            out.write(imageData);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to write image", e);
+            return false;
+        } finally {
+            try {
+                out.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-
         return true;
     }
     
@@ -95,22 +111,24 @@ public class CameraPreviewView extends SurfaceView implements SurfaceHolder.Call
         mCamera = Camera.open();
         try {
            mCamera.setPreviewDisplay(holder);
-           Parameters param = mCamera.getParameters();
+           /*Parameters param = mCamera.getParameters();
            param.setRotation(180);
-           mCamera.setParameters(param);
+           mCamera.setParameters(param);*/
+           
+           mCamera.setDisplayOrientation(90);
            
            // support auto focus
-            mCamera.autoFocus(new Camera.AutoFocusCallback() {
-                Camera.ShutterCallback shutterCallback = new Camera.ShutterCallback() {
-                    public void onShutter() {
-                        // Play your sound here.
-                    }
-                };
-
-                public void onAutoFocus(boolean success, Camera camera) {
-                    camera.takePicture(shutterCallback, null, mPictureCallback);
-                }
-            });
+//            mCamera.autoFocus(new Camera.AutoFocusCallback() {
+//                Camera.ShutterCallback shutterCallback = new Camera.ShutterCallback() {
+//                    public void onShutter() {
+//                        // Play your sound here.
+//                    }
+//                };
+//
+//                public void onAutoFocus(boolean success, Camera camera) {
+//                    camera.takePicture(shutterCallback, null, mPictureCallback);
+//                }
+//            });
         } catch (IOException exception) {
             mCamera.release();
             mCamera = null;
@@ -172,6 +190,55 @@ public class CameraPreviewView extends SurfaceView implements SurfaceHolder.Call
 
         mCamera.setParameters(parameters);
         mCamera.startPreview();
+    }
+    
+    private class MyOrientationEventListener extends OrientationEventListener {
+        public MyOrientationEventListener(Context context) {
+            super(context);
+        }
+
+        @Override
+        public void onOrientationChanged(int orientation) {
+            /*// We keep the last known orientation. So if the user first orient
+            // the camera then point the camera to floor or sky, we still have
+            // the correct orientation.
+            if (orientation == ORIENTATION_UNKNOWN)
+                return;
+            int newOrientation = Util.roundOrientation(orientation,
+                    mOrientation);
+
+            if (mOrientation != newOrientation) {
+                mOrientation = newOrientation;
+                // The input of effects recorder is affected by
+                // android.hardware.Camera.setDisplayOrientation. Its value only
+                // compensates the camera orientation (no Display.getRotation).
+                // So the orientation hint here should only consider sensor
+                // orientation.
+                if (effectsActive()) {
+                    mEffectsRecorder.setOrientationHint(mOrientation);
+                }
+            }
+
+            // When the screen is unlocked, display rotation may change. Always
+            // calculate the up-to-date orientationCompensation.
+            int orientationCompensation = (mOrientation + Util
+                    .getDisplayRotation(VideoCamera.this)) % 360;
+
+            if (mOrientationCompensation != orientationCompensation) {
+                mOrientationCompensation = orientationCompensation;
+                // Do not rotate the icons during recording because the video
+                // orientation is fixed after recording.
+                if (!mMediaRecorderRecording) {
+                    setOrientationIndicator(mOrientationCompensation, true);
+                }
+            }
+
+            // Show the toast after getting the first orientation changed.
+            if (mHandler.hasMessages(SHOW_TAP_TO_SNAPSHOT_TOAST)) {
+                mHandler.removeMessages(SHOW_TAP_TO_SNAPSHOT_TOAST);
+                showTapToSnapshotToast();
+            }*/
+        }
     }
 
 }
